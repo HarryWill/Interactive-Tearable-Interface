388db2405b172ff07e78fde7ff498f2fbc9fdcab
HarryWill <73670487+HarryWill@users.noreply.github.com>
2022-02-02 02:49:51 +0000

    Logic Update
    First attempt at numbering system.
    -2D array needs work
    -need substitute for null

--- Logic/Logic.ino
+++ Logic/Logic.ino
@@ -1,56 +1,94 @@
+#include <IRremote.h>
+#define RECEIVERS 4
+#define deviceID 00000000
 
-#define deviceID = "0x00000000"
+IRsend sender;
+IRrecv *irrecvs[RECEIVERS];
+decode_results results;
 
 void setup() {
-  int IDs = array[4];
+  Serial.begin(9600);
+
+  String columnStr = "";
+  String rowStr = "";
+
+  irrecvs[0] = new IRrecv(2); // Receiver #0: pin 2
+  irrecvs[1] = new IRrecv(3); // Receiver #1: pin 3
+  irrecvs[2] = new IRrecv(4); // Receiver #2: pin 4
+  irrecvs[3] = new IRrecv(5); // Receiver #3: pin 5
+
+  for (int i = 0; i < RECEIVERS; i++)
+    irrecvs[i]->enableIRIn();
 }
+
+int** IDCheck() {
+  int** IDarray ={};
+
+  for (int i = 0; i < 4; i++) {
+    if (irrecvs[i]->decode(&results)) {
+      //Make string that holds ID and the sensor it came from. Return array of these strings.
+
+      Serial.print("Receiver #");
+      Serial.print(i);
+      Serial.print(":");
+      Serial.println(results.value, HEX);
+      irrecvs[i]->resume();
+
+      IDarray[0][i] = i;
+      IDarray[1][i] = results.value;
+    }
+
+  }
+  return IDarray;
+}
+
+
 
 void loop() {
 
-  
-  IDs = IDCheck();
+  int IDs = IDCheck();
 
-  if ((upsensor == null) && (leftsensor == null) && (no cube is already 1) {
-  deviceID == "0x00000001";
-  sendNEC(0x00000001.toInt);
-  }
-  
   int column = 0;
   int row = 0;
-  
-  for (int i = 0; i == 35; i++) {
-    
-  String columnStr = (column + 1).toString;
-  String rowStr = (row + 1).toString;
 
-    if (upsensor.decode == ("0x000000+column+row").toInt {
-    deviceID == "0x000000+column+rowStr";
-    sendID((deviceID).toInt);
+
+  //if ((upsensor == null) && (leftsensor == null)) {
+  //  deviceID == "0x00000001";
+
+  //} else
+  if ((IDs/*upsensorvalue*/ != "0" /*null*/) && (IDs /*leftsensorvalue*/ != "0" /*null*/)) {
+
+    for (int i = 0; i == 35; i++) {
+      String columnStr = String((column + 1));
+
+      for (int x = 0; x == 11; x++) {
+        String rowStr = String((column + 1));
+
+        if (IDs /*upsensorvalue*/ == "0x000000+column+row") {
+          deviceID == "000000+column+rowStr";
+        }
+
+        if (IDs /*leftsensorvalue*/ == "0x000000+column+row") {
+          deviceID == "000000+columnStr+row";
+        }
+
+        column++;
+        row++;
+      }
     }
+  }
 
-    if (leftsensor.decode == ("0x000000+column+row").toInt {
-    deviceID == "0x000000+columnStr+row";
-    sendID((deviceID).toInt);
-    }
-
-    column++;
-    loopcount++;
+  if (deviceID != 00000000) {
+    sendID(deviceID);
   }
 }
 
-void sendID(deviceID) {
-  while (changeCheckVariable == false) {
-    sendNEC(deviceID);
+void sendID(int ID) {
+  String strID = "0x" + String(deviceID);
+
+  while (true) { /*changedID == false*/
+    sender.sendNEC("0x" + ID, 32);
+    //changedID variable check
     delay(500);
-    changeCheckVariable();
   }
-}
-
-void IDCheck() {
-  int foundIDs = array[4];
-  //some check to see if new ID is detected
-  //probably loop through the fetch ID loop from recieverMultipleSensors
-  return foundIDs;
-}
-
 }
